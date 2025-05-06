9ccb101fa264e86dfc3b660d5599dce0c6491aa0
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2025-05-06 16:23:59 +0200

    3.16: Version bump

--- rpgmtl.py
+++ rpgmtl.py
@@ -68,7 +68,7 @@ class PatcherHelper():
 ######################################################
 class RPGMTL():
     # constant
-    VERSION = "3.15"
+    VERSION = "3.16"
     CHILDREN_FILE_ID = "@__children_file__@:"
     HISTORY_LIMIT = 10
     def __init__(self : RPGMTL) -> None:
