55656c3d8fe1240c15a501db3f9c70242204577f
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2025-12-14 14:22:02 +0100

    Added a more generic text wrap tool

--- plugins/general.py
+++ plugins/general.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from . import Plugin, GloIndex, LocIndex, IntBool
 from typing import Any
+import textwrap
 
 class GeneralActions(Plugin):
     def __init__(self : GeneralActions) -> None:
@@ -23,11 +24,119 @@ class GeneralActions(Plugin):
 
     def get_tool_infos(self : GeneralActions) -> dict[str, list]:
         return {
-            "general_clear_all_modified": ["assets/images/update.png", "Clear All Modified flags", self.clear_all, {"type":self.SIMPLE_TOOL}]
+            "general_clear_all_modified": [
+                "assets/images/update.png", "Clear All Modified flags", self.clear_all, {"type":self.SIMPLE_TOOL}
+            ],
+            "general_text_wrap": [
+                "assets/images/text_wrap.png", "Generic Text wrap", self.tool_text_wrap,
+                {
+                    "type":self.COMPLEX_TOOL,
+                    "params":{
+                        "_t_char_limit":["Character Limit", "num", 60, None],
+                        "_t_space":["Fill with spaces instead of using a newline:", "bool", False, None],
+                        "_t_file_ext":["Only on files ending with (Separate by ,)(Optional):", "str", "", None],
+                        "_t_0000":["You can ignore the start of a string:", "display", None, None],
+                        "_t_start":["Starting with (Optional):", "str", "", None],
+                        "_t_end":["and ending with:", "str", "", None],
+                    },
+                    "help":"Tool to automatically wrap texts of RPG Maker games."
+                }
+            ],
         }
 
     def match(self : GeneralActions, file_path : str, is_for_action : bool) -> bool:
         return is_for_action
+
+    def tool_text_wrap(self : GeneralActions, name : str, params : dict[str, Any]) -> str:
+        try:
+            limit : int = int(params["_t_char_limit"])
+            if limit < 1:
+                raise Exception()
+        except:
+            return "Invalid character limit, it must be a positive integer."
+        try:
+            extensions : tuple[str] = tuple(params["_t_file_ext"].split(","))
+        except:
+            return "Failed to parse file ending setting."
+        try:
+            self.owner.save() # save first!
+            self.owner.backup_strings_file(name) # backup strings.json
+            self.owner.load_strings(name)
+            seen : set[str] = set() # used to track which strings we tested
+            count : int = 0
+            for file in self.owner.strings[name]["files"]:
+                if len(extensions) > 0 and not file.endswith(extensions):
+                    continue
+                
+                for i, group in enumerate(self.owner.strings[name]["files"][file]):
+                    for j in range(1, len(group)):
+                        sid : str = self.owner.strings[name]["files"][file][i][j][LocIndex.ID]
+                        if self.owner.strings[name]["strings"][sid][GloIndex.TL] is not None:
+                            if sid not in seen:
+                                seen.add(sid)
+                                s, b = self._tool_text_wrap_sub(
+                                    limit,
+                                    self.owner.strings[name]["strings"][sid][GloIndex.TL],
+                                    params["_t_start"],
+                                    params["_t_end"],
+                                    params["_t_space"]
+                                )
+                                if b:
+                                    self.owner.modified[name] = True
+                                    self.owner.strings[name]["strings"][sid][GloIndex.TL] = s
+                                    count += 1
+                        
+                        if self.owner.strings[name]["files"][file][i][j][LocIndex.TL] is not None:
+                            s, b = self._tool_text_wrap_sub(
+                                limit,
+                                self.owner.strings[name]["strings"][sid][LocIndex.TL],
+                                params["_t_start"],
+                                params["_t_end"],
+                                params["_t_space"]
+                            )
+                            if b:
+                                self.owner.modified[name] = True
+                                self.owner.strings[name]["files"][file][i][j][LocIndex.TL] = s
+                                count += 1
+            if count == 0:
+                return "No strings have been modified"
+            else:
+                return str(count) + " strings have been wrapped"
+        except Exception as e:
+            self.owner.log.error("[JSON] Tool 'tool_text_wrap' failed with error:\n" + self.owner.trbk(e))
+            return "An unexpected error occured"
+
+    def _tool_text_wrap_sub(
+        self : GeneralActions,
+        limit : int,
+        string : str,
+        start_delim : str,
+        end_delim : str,
+        use_space : bool
+    ) -> tuple[str, bool]:
+        start : str = ""
+        old : str = string
+        if end_delim != "":
+            if start_delim == "" or string.startswith(start_delim):
+                split = string.split(end_delim, 1)
+                if len(split) == 2:
+                    start, string = split
+                    start += end_delim
+        if len(string) <= limit:
+            return "", False
+        p : list[str] = [
+            s
+            for s in string.replace("\n", " ").split(" ")
+            if s != ""
+        ]
+        p = textwrap.wrap(" ".join(p), width=limit, break_on_hyphens=False)
+        if use_space:
+            for i in range(len(p) - 1):
+                p[i] = p[i].ljust(limit)
+            string = start + "".join(p)
+        else:
+            string = start + "\n".join(p)
+        return string, string != old
 
     def check_limit(self : GeneralActions, name : str, file_path : str, settings : dict[str, Any] = {}) -> str:
         try:
