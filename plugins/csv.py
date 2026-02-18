c7b033e731f6d8782be4dc60dfc6b4f0b48b4f0f
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2026-02-18 22:43:23 +0100

    Support for pylivemaker CSV

--- plugins/csv.py
+++ plugins/csv.py
@@ -4,10 +4,13 @@ import csv
 import io
 
 class CSV(Plugin):
+    LM_JP_TEXT = 3
+    LM_EN_TEXT = 4
+    
     def __init__(self : CSV) -> None:
         super().__init__()
         self.name : str = "CSV"
-        self.description : str = " v1.5\nHandle CSV files, including externMessage files from RPG Maker MV/MZ"
+        self.description : str = " v2.0\nHandle CSV files, including externMessage files from RPG Maker MV/MZ"
         self.related_tool_plugins : list[str] = [self.name]
 
     def match(self : CSV, file_path : str, is_for_action : bool) -> bool:
@@ -19,7 +22,19 @@ class CSV(Plugin):
             "csv_ignore_non_string": ["Ignore empty strings and numbers (Require re-extract)", "bool", True, None],
         }
 
+    def is_livemaker_csv(self : CSV, content : bytes) -> bool:
+        return (
+            content.startswith(b"ID,Label,Context,Original text,Translated text")
+            and b"pylm" in content
+        )    
+
     def read(self : CSV, file_path : str, content : bytes) -> list[list[str]]:
+        if self.is_livemaker_csv(content):
+            return self.read_livemaker(file_path, content)
+        else:
+            return self.read_standard(file_path, content)
+    
+    def read_standard(self : CSV, file_path : str, content : bytes) -> list[list[str]]:
         entries : list[list[str]] = []
         with io.StringIO(self.decode(content)) as sin:
             reader = csv.reader(sin)
@@ -57,7 +72,33 @@ class CSV(Plugin):
                             entries.append([f"Cell {i}x{j}", cell])
         return entries
 
+    def read_livemaker(self : CSV, file_path : str, content : bytes) -> list[list[str]]:
+        entries : list[list[str]] = []
+        with io.StringIO(self.decode(content)) as sin:
+            reader = csv.reader(sin)
+            group = [""]
+            for i, row in enumerate(reader):
+                if i == 0:
+                    continue
+                g = row[0].split(".lsb:", 1)[1].split(":")[0]
+                if g != group[0]:
+                    if len(group) > 1:
+                        entries.append(group)
+                        group = [g]
+                    else:
+                        group[0] = g
+                group.append(row[self.LM_JP_TEXT])
+            if len(group) > 1:
+                entries.append(group)
+        return entries
+
     def write(self : CSV, name : str, file_path : str, content : bytes) -> tuple[bytes, bool]:
+        if self.is_livemaker_csv(content):
+            return self.write_livemaker(name, file_path, content)
+        else:
+            return self.write_standard(name, file_path, content)
+        
+    def write_standard(self : CSV, name : str, file_path : str, content : bytes) -> tuple[bytes, bool]:
         helper : WalkHelper = WalkHelper(file_path, self.owner.strings[name])
         with io.StringIO(self.decode(content)) as sin:
             reader = csv.reader(sin)
@@ -87,6 +128,29 @@ class CSV(Plugin):
                     for j, cell in enumerate(row):
                         if not ignore_ns or (cell != "" and not cell.isdigit()):
                             content[i][j] = helper.apply_string(content[i][j], f"Cell {i}x{j}")
+        # write
+        with io.StringIO() as sout:
+            writer = csv.writer(sout, delimiter=',', lineterminator='\n')
+            for i, row in enumerate(content):
+                writer.writerow(content[i])
+            sout.seek(0)
+            if helper.modified:
+                return self.encode(sout.read()), True
+            else:
+                return content, False
+        
+    def write_livemaker(self : CSV, name : str, file_path : str, content : bytes) -> tuple[bytes, bool]:
+        helper : WalkHelper = WalkHelper(file_path, self.owner.strings[name])
+        with io.StringIO(self.decode(content)) as sin:
+            reader = csv.reader(sin)
+            content = [row for row in reader]
+        for i, row in enumerate(content):
+            if i == 0:
+                continue
+            group = row[0].split(".lsb:", 1)[1].split(":")[0]
+            tl = helper.apply_string(row[self.LM_JP_TEXT], group)
+            if tl != row[self.LM_JP_TEXT]:
+                content[i][self.LM_EN_TEXT] = tl
         # write
         with io.StringIO() as sout:
             writer = csv.writer(sout, delimiter=',', lineterminator='\n')
