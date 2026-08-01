c4822b9e96b651a8491dff57651db36a6fba9e8b
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2026-08-01 12:47:18 +0200

    Changed RPGMTL version to 4.0

--- rpgmtl.py
+++ rpgmtl.py
@@ -65,7 +65,7 @@ class PatcherHelper():
 ######################################################
 class RPGMTL():
     # constant
-    VERSION = "3.31"
+    VERSION = "4.0"
     CHILDREN_FILE_ID = "@__children_file__@:"
     HISTORY_LIMIT = 10
     CURRENT_CONFIG_VERSION = 3
